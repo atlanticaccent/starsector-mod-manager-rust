4f7b3c96b75e865085a5aa10d2ed34777b66ed33
Iain Laird <iain@laird.org>
2021-01-23 16:01:53 +0000

    Refactored archive code into module
    Also refactored archive handling code to properly handle/propogate
    errors.

--- src/archive_handler.rs
+++ src/archive_handler.rs
@@ -1,25 +1,31 @@
-use std::fs;
-use std::io::Write;
-use std::path::Path;
+use std::{fs, fmt, error::Error, path::Path};
 
-fn main() {
-  let args = std::env::args();
-  let mut stderr = std::io::stderr();
-  let file = args.skip(1).next().unwrap_or_else(|| {
-    writeln!(&mut stderr, "Please pass an archive as argument!").unwrap();
-    std::process::exit(0)
-  });
-  
+struct UnrarErr<T> {
+  err: unrar::error::UnrarError<T>,
+}
+
+impl<T> fmt::Display for UnrarErr<T> {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    return self.err.fmt(f);
+  }
+}
+
+impl<T> fmt::Debug for UnrarErr<T> {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    return self.err.fmt(f);
+  }
+}
+
+impl<T> Error for UnrarErr<T> {}
+
+pub fn handle_archive(file: &String) -> Result<bool, Box<dyn Error>>{
   let kind = match infer::get_from_path(&file) {
     Ok(res) => 
       match res {
         Some(kind) => kind,
-        None => return
+        None => return Ok(false),
       },
-    Err(_) => {
-      println!("Not a recognizable file type");
-      return;
-    }
+    Err(err) => return Err(Box::new(err))
   };
   
   match kind.mime_type() {
@@ -28,49 +34,77 @@ fn main() {
         .join(&file)
         .to_string_lossy()
         .to_string();
-      unrar::Archive::new(file.clone())
-        .extract_to(output_dir)
-        .unwrap()
-        .process()
-        .unwrap();
-      println!("Done.");
+
+      match unrar::Archive::new(file.clone()).extract_to(output_dir) {
+        Ok(mut res) => match res.process() {
+          Ok(_) => return Ok(true),
+          Err(err) => return Err(Box::new(UnrarErr { err })),
+        },
+        Err(err) => return Err(Box::new(UnrarErr { err })),
+      }
     },
     "application/zip" => {
-      let mut archive = zip::ZipArchive::new(fs::File::open(file).unwrap()).unwrap();
-      
-      for i in 0..archive.len() {
-        let mut file = archive.by_index(i).unwrap();
-        let outpath = match file.enclosed_name() {
-          Some(path) => Path::new("./temp").join(path.to_owned()),
-          None => continue,
-        };
-        
-        if (&*file.name()).ends_with('/') {
-          println!("File {} extracted to \"{}\"", i, outpath.display());
-          fs::create_dir_all(&outpath).unwrap();
-        } else {
-          println!(
-            "File {} extracted to \"{}\" ({} bytes)",
-            i,
-            outpath.display(),
-            file.size()
-          );
-          if let Some(p) = outpath.parent() {
-            if !p.exists() {
-              fs::create_dir_all(&p).unwrap();
+      match fs::File::open(file) {
+        Ok(reader) => match zip::ZipArchive::new(reader) {
+          Ok(mut archive) => {
+            for i in 0..archive.len() {
+              match archive.by_index(i) {
+                Ok(mut file) => {
+                  let outpath = match file.enclosed_name() {
+                    Some(path) => Path::new("./temp").join(path.to_owned()),
+                    None => continue,
+                  };
+                  
+                  if (&*file.name()).ends_with('/') {
+                    println!("File {} extracted to \"{}\"", i, outpath.display());
+                    if let Err(err) = fs::create_dir_all(&outpath) {
+                      return Err(Box::new(err));
+                    }
+                  } else {
+                    println!(
+                      "File {} extracted to \"{}\" ({} bytes)",
+                      i,
+                      outpath.display(),
+                      file.size()
+                    );
+                    if let Some(p) = outpath.parent() {
+                      if !p.exists() {
+                        if let Err(err) = fs::create_dir_all(&p) {
+                          return Err(Box::new(err));
+                        }
+                      }
+                    }
+                    match fs::File::create(&outpath) {
+                      Ok(mut outfile) => if let Err(err) = std::io::copy(&mut file, &mut outfile) {
+                        return Err(Box::new(err));
+                      },
+                      Err(err) => return Err(Box::new(err))
+                    }
+                  }
+                },
+                Err(err) => return Err(Box::new(err))
+              }
             }
+
+            return Ok(true);
           }
-          let mut outfile = fs::File::create(&outpath).unwrap();
-          std::io::copy(&mut file, &mut outfile).unwrap();
-        }
+          Err(err) => return Err(Box::new(err))
+        },
+        Err(err) => return Err(Box::new(err))
       }
     },
     "application/x-7z-compressed" => {
       let mut source = fs::File::open(&file).expect("Could not open file");
       let dest = Path::new("./tmp/dest");
       
-      compress_tools::uncompress_archive(&mut source, &dest, compress_tools::Ownership::Ignore).expect("Could not decompress");
+      match compress_tools::uncompress_archive(&mut source, &dest, compress_tools::Ownership::Ignore) {
+        Ok(()) => return Ok(true),
+        Err(err) => return Err(Box::new(err)),
+      }
     },
-    _ => println!("is something else"),
+    _ => {
+      println!("is something else");
+      return Ok(false);
+    },
   }
 }
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io::Write;
+
+mod archive_handler;
+
+fn main() {
+  let args = std::env::args();
+  let mut stderr = std::io::stderr();
+  let file = args.skip(1).next().unwrap_or_else(|| {
+    writeln!(&mut stderr, "Please pass an archive as argument!").unwrap();
+    std::process::exit(0)
+  });
+
+  match archive_handler::handle_archive(&file) {
+      Ok(res) => println!("Ran without exception. Completed? {}", res),
+      Err(err) => println!("Encountered exception {}", err),
+  }
+}
