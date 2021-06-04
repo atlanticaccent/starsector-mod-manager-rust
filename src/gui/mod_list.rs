22223b42ff83a55eaa15aa62979fabb72aca9734
Iain Laird <ikl@st-andrews.ac.uk>
2021-06-04 15:51:18 +0100

    Remove unneccessary unpacking

--- src/gui/mod_list.rs
+++ src/gui/mod_list.rs
@@ -272,7 +272,7 @@ impl ModList {
       let enabled_mods_filename = mod_dir.join("enabled_mods.json");
       // Note: If the enabled_mods.json file does not exist or is malformed, this entire function call fails.
       if let Ok(enabled_mods_text) = std::fs::read_to_string(enabled_mods_filename);
-      if let Ok(EnabledMods { enabled_mods, .. }) = serde_json::from_str::<EnabledMods>(&enabled_mods_text);
+      if let Ok(EnabledMods { enabled_mods }) = serde_json::from_str::<EnabledMods>(&enabled_mods_text);
       // Whilst that shouldn't happen (Starsector should make the file) manual deletion, manual instantiation of the mods folder, or some other error, can cause this to go poorly - consider generating ourselves.
       if let Ok(dir_iter) = std::fs::read_dir(mod_dir);
       then {
