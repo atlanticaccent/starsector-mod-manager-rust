fcf6e3b3b76fd614137d8415a7c18a65bfe6cf5d
Iain Laird <ikl@st-andrews.ac.uk>
2021-10-29 17:27:07 +0100

    Add error messaging when fail to parse unpacked mod_info
    No longer silently fails when the manager either fails to find a mod in
    the folder unpacked from a given archive (ie: it never contained a mod)
    or the mod_info.json file contained in the unpacked mod is malformed
    sufficiently that it cannot be parsed.

--- src/gui/installer.rs
+++ src/gui/installer.rs
@@ -18,6 +18,7 @@ use snafu::{Snafu, ResultExt, OptionExt};
 use remove_dir_all::remove_dir_all;
 use infer;
 use unrar;
+use if_chain::if_chain;
 
 use super::mod_list::ModEntry;
 
@@ -198,29 +199,32 @@ async fn handle_path(tx: mpsc::UnboundedSender<ChannelMessage>, path: PathBuf, m
   };
 
   let dir = mod_folder.get_path_copy();
-  if let Ok(maybe_path) = task::spawn_blocking(move || find_nested_mod(&dir)).await.expect("Find mod in given folder") {
-    if let Some(mod_path) = maybe_path {
-      if let Ok(mod_info) = ModEntry::from_file(mod_path.join("mod_info.json")) {
-        if let Some(id) = installed.into_iter().find(|existing| **existing == mod_info.id) {
-          mod_folder = match mod_folder {
-            HybridPath::PathBuf(_) => HybridPath::PathBuf(mod_path.clone()),
-            HybridPath::Temp(temp, _) => HybridPath::Temp(temp, Some(mod_path.clone()))
-          };
+  if_chain! {
+    if let Ok(maybe_path) = task::spawn_blocking(move || find_nested_mod(&dir)).await.expect("Find mod in given folder");
+    if let Some(mod_path) = maybe_path;
+    if let Ok(mod_info) = ModEntry::from_file(mod_path.join("mod_info.json"));
+    then {
+      if let Some(id) = installed.into_iter().find(|existing| **existing == mod_info.id) {
+        mod_folder = match mod_folder {
+          HybridPath::PathBuf(_) => HybridPath::PathBuf(mod_path.clone()),
+          HybridPath::Temp(temp, _) => HybridPath::Temp(temp, Some(mod_path.clone()))
+        };
 
-          tx.send(ChannelMessage::Duplicate(mod_info.name, id, mod_folder, None)).expect("Send query over async channel");
-        } else if !mods_dir.join(mod_info.id.clone()).exists() {
-          move_or_copy(mod_path, mods_dir.join(mod_info.id)).await;
+        tx.send(ChannelMessage::Duplicate(mod_info.name, id, mod_folder, None)).expect("Send query over async channel");
+      } else if !mods_dir.join(mod_info.id.clone()).exists() {
+        move_or_copy(mod_path, mods_dir.join(mod_info.id)).await;
 
-          tx.send(ChannelMessage::Success(mod_info.name)).expect("Send success over async channel");
-        } else {
-          mod_folder = match mod_folder {
-            HybridPath::PathBuf(_) => HybridPath::PathBuf(mod_path.clone()),
-            HybridPath::Temp(temp, _) => HybridPath::Temp(temp, Some(mod_path.clone()))
-          };
+        tx.send(ChannelMessage::Success(mod_info.name)).expect("Send success over async channel");
+      } else {
+        mod_folder = match mod_folder {
+          HybridPath::PathBuf(_) => HybridPath::PathBuf(mod_path.clone()),
+          HybridPath::Temp(temp, _) => HybridPath::Temp(temp, Some(mod_path.clone()))
+        };
 
-          tx.send(ChannelMessage::Duplicate(mod_info.name, String::new(), mod_folder, Some(mods_dir.join(mod_info.id.clone())))).expect("Send query over async channel");
-        }
+        tx.send(ChannelMessage::Duplicate(mod_info.name, String::new(), mod_folder, Some(mods_dir.join(mod_info.id.clone())))).expect("Send query over async channel");
       }
+    } else {
+      tx.send(ChannelMessage::Error(format!("Could not find mod folder or parse mod_info file."))).expect("Send error over async channel");
     }
   }
 }
