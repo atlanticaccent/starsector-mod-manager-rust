1f63d905a893c43d8e3161264186f62765ce93bd
Iain Laird <iain@laird.org>
2022-05-08 19:57:22 +0100

    Implement vmparams tests

--- src/app/settings/vmparams.rs
+++ src/app/settings/vmparams.rs
@@ -1,18 +1,20 @@
 use druid::{Data, Lens};
 use if_chain::if_chain;
-use strum_macros::EnumIter;
-use std::fmt::Display;
 use std::iter::Peekable;
+use std::marker::PhantomData;
 use std::path::PathBuf;
 use std::str::Chars;
+use std::{fmt::Display, path::Path};
+use strum_macros::EnumIter;
 
 use crate::app::util::{LoadError, SaveError};
 
 #[derive(Debug, Clone, Data, Lens)]
-pub struct VMParams {
+pub struct VMParams<T = PlatformDefault> {
   pub heap_init: Value,
   pub heap_max: Value,
   pub thread_stack_size: Value,
+  _phantom: PhantomData<T>,
 }
 
 #[derive(Debug, Clone, Data, Lens)]
@@ -54,17 +56,24 @@ impl Display for Unit {
   }
 }
 
-impl VMParams {
+pub trait PlatformVMParamsPath {
   fn path() -> PathBuf {
     PathBuf::from(r"./vmparams")
   }
+}
 
-  pub fn load(install_dir: PathBuf) -> Result<VMParams, LoadError> {
+#[derive(Debug, Clone, Data)]
+pub struct PlatformDefault;
+
+impl PlatformVMParamsPath for PlatformDefault {}
+
+impl<T: PlatformVMParamsPath> VMParams<T> {
+  pub fn load(install_dir: impl AsRef<Path>) -> Result<VMParams<T>, LoadError> {
     use std::fs;
     use std::io::Read;
 
     let mut params_file =
-      fs::File::open(install_dir.join(VMParams::path())).map_err(|_| LoadError::NoSuchFile)?;
+      fs::File::open(install_dir.as_ref().join(T::path())).map_err(|_| LoadError::NoSuchFile)?;
 
     let mut params_string = String::new();
     params_file
@@ -96,11 +105,13 @@ impl VMParams {
         }
       };
 
-      match &param[..4] {
-        "-Xms" | "-xms" => heap_init = parse_pair()?,
-        "-Xmx" | "-xmx" => heap_max = parse_pair()?,
-        "-Xss" | "-xss" => thread_stack_size = parse_pair()?,
-        _ => {}
+      if let Some(slice) = param.get(..4) {
+        match slice {
+          "-Xms" | "-xms" => heap_init = parse_pair()?,
+          "-Xmx" | "-xmx" => heap_max = parse_pair()?,
+          "-Xss" | "-xss" => thread_stack_size = parse_pair()?,
+          _ => {}
+        }
       }
     }
 
@@ -111,18 +122,19 @@ impl VMParams {
         heap_init,
         heap_max,
         thread_stack_size,
+        _phantom: PhantomData::default(),
       })
     } else {
       Err(LoadError::FormatError)
     }
   }
 
-  pub fn save(self, install_dir: PathBuf) -> Result<(), SaveError> {
+  pub fn save(self, install_dir: impl AsRef<Path>) -> Result<(), SaveError> {
     use std::fs;
     use std::io::{Read, Write};
 
     let mut params_file =
-      fs::File::open(install_dir.join(VMParams::path())).map_err(|_| SaveError::Format)?;
+      fs::File::open(install_dir.as_ref().join(T::path())).map_err(|_| SaveError::Format)?;
 
     let mut params_string = String::new();
     params_file
@@ -135,17 +147,17 @@ impl VMParams {
       match ch {
         '-' => match input_iter.clone().take(4).collect::<String>().as_str() {
           key @ "-Xms" | key @ "-xms" => {
-            VMParams::consume_value(&mut input_iter)?;
+            VMParams::<T>::consume_value(&mut input_iter)?;
             output.push_str(key);
             output.push_str(&self.heap_init.to_string())
           }
           key @ "-Xmx" | key @ "-xmx" => {
-            VMParams::consume_value(&mut input_iter)?;
+            VMParams::<T>::consume_value(&mut input_iter)?;
             output.push_str(key);
             output.push_str(&self.heap_max.to_string())
           }
           key @ "-Xss" | key @ "-xss" => {
-            VMParams::consume_value(&mut input_iter)?;
+            VMParams::<T>::consume_value(&mut input_iter)?;
             output.push_str(key);
             output.push_str(&self.thread_stack_size.to_string())
           }
@@ -163,7 +175,7 @@ impl VMParams {
     }
 
     let mut file =
-      fs::File::create(install_dir.join(VMParams::path())).map_err(|_| SaveError::File)?;
+      fs::File::create(install_dir.as_ref().join(T::path())).map_err(|_| SaveError::File)?;
 
     file
       .write_all(output.as_bytes())
@@ -199,5 +211,118 @@ impl VMParams {
         Err(SaveError::Format)
       }
     }
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use std::{io::Seek, marker::PhantomData, path::PathBuf, sync::Mutex};
+
+  use crate::app::settings::vmparams::{PlatformVMParamsPath, VMParams};
+
+  lazy_static::lazy_static! {
+    static ref TEST_FILE: tempfile::NamedTempFile = tempfile::NamedTempFile::new().expect("Couldn't create tempdir - not a real test failure");
+
+    static ref ROOT: PathBuf = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests").join("assets");
+
+    static ref DUMB_MUTEX: Mutex<()> = Mutex::new(());
+  }
+
+  struct TempPath;
+
+  impl PlatformVMParamsPath for TempPath {
+    fn path() -> PathBuf {
+      TEST_FILE.path().to_path_buf()
+    }
+  }
+
+  fn test_func<T: PlatformVMParamsPath>() {
+    let _guard = DUMB_MUTEX.lock().expect("Lock dumb mutex");
+
+    let root = ROOT.as_path();
+
+    let vmparams = VMParams::<T>::load(root);
+
+    assert!(vmparams.is_ok());
+
+    if let Ok(mut vmparams) = vmparams {
+      println!(
+        "{:?} {:?} {:?}",
+        vmparams.heap_init, vmparams.heap_max, vmparams.thread_stack_size
+      );
+
+      vmparams.heap_init.amount = 2048;
+      vmparams.heap_max.amount = 2048;
+      vmparams.thread_stack_size.amount = 4096;
+
+      let mut reader =
+        std::fs::File::open(root.join(T::path())).expect("Open original file for reading");
+
+      let mut testfile = TEST_FILE.as_file().clone();
+      testfile.set_len(0).expect("Truncate");
+      testfile.rewind().expect("Truncate");
+
+      std::io::copy(&mut reader, &mut testfile).expect("Copy vmparams to tempfile");
+
+      let edited_vmparams = VMParams::<TempPath> {
+        heap_init: vmparams.heap_init,
+        heap_max: vmparams.heap_max,
+        thread_stack_size: vmparams.thread_stack_size,
+        _phantom: PhantomData::default(),
+      };
+
+      let res = edited_vmparams.save(PathBuf::from("/"));
+
+      assert!(res.is_ok());
+
+      let edited_vmparams = VMParams::<TempPath>::load(PathBuf::from("/"));
+
+      assert!(edited_vmparams.is_ok());
+
+      if let Ok(edited_vmparams) = edited_vmparams {
+        assert!(edited_vmparams.heap_init.amount == 2048);
+        assert!(edited_vmparams.heap_max.amount == 2048);
+        assert!(edited_vmparams.thread_stack_size.amount == 4096);
+      }
+    }
+  }
+
+  #[test]
+  fn test_windows() {
+    struct Windows;
+
+    impl PlatformVMParamsPath for Windows {
+      fn path() -> PathBuf {
+        PathBuf::from("./vmparams_windows")
+      }
+    }
+
+    test_func::<Windows>();
+  }
+
+  #[test]
+  fn test_linux() {
+    struct Linux;
+
+    impl PlatformVMParamsPath for Linux {
+      fn path() -> PathBuf {
+        PathBuf::from("./vmparams_linux")
+      }
+    }
+
+    test_func::<Linux>();
+  }
+
+  #[test]
+  fn test_macos() {
+    struct MacOS;
+
+    impl PlatformVMParamsPath for MacOS {
+      fn path() -> PathBuf {
+        PathBuf::from("./vmparams_macos")
+      }
+    }
+
+    test_func::<MacOS>();
   }
 }
