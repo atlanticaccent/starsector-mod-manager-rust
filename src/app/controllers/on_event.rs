d16356596f8dab27cd141d5e8ce64c6950dfc0f9
Iain Laird <iain@laird.org>
2023-12-04 15:52:03 +0000

    Pass widget in convenience event controller

--- src/app/controllers/on_event.rs
+++ src/app/controllers/on_event.rs
@@ -1,22 +1,22 @@
 use druid::widget::prelude::*;
 use druid::widget::Controller;
 
-pub struct OnEvent<T> {
-  handler: Box<dyn Fn(&mut EventCtx, &Event, &mut T) -> bool>,
+pub struct OnEvent<T, W: Widget<T>> {
+  handler: Box<dyn Fn(&mut W, &mut EventCtx, &Event, &mut T) -> bool>,
 }
 
 #[allow(dead_code)]
-impl<T> OnEvent<T> {
-  pub fn new(handler: impl Fn(&mut EventCtx, &Event, &mut T) -> bool + 'static) -> Self {
+impl<T, W: Widget<T>> OnEvent<T, W> {
+  pub fn new(handler: impl Fn(&mut W, &mut EventCtx, &Event, &mut T) -> bool + 'static) -> Self {
     Self {
       handler: Box::new(handler),
     }
   }
 }
 
-impl<T: Data, W: Widget<T>> Controller<T, W> for OnEvent<T> {
+impl<T: Data, W: Widget<T>> Controller<T, W> for OnEvent<T, W> {
   fn event(&mut self, child: &mut W, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
-    if (self.handler)(ctx, event, data) {
+    if (self.handler)(child, ctx, event, data) {
       ctx.set_handled();
     }
     child.event(ctx, event, data, env);
