9b29de751cb09b72289e08f1efb4313d512137f8
Iain Laird <iain.laird@truelayer.com>
2024-01-15 15:18:12 +0000

    Rework icon imports

--- src/icons/mod.rs
+++ src/icons/mod.rs
@@ -1,11 +1,47 @@
-pub use druid_widget_nursery::material_icons::normal::{
-  action::{EXTENSION, HELP, INSTALL_DESKTOP, OPEN_IN_BROWSER as OPEN_BROWSER, SETTINGS, VERIFIED},
-  av::{NEW_RELEASES, PLAY_ARROW},
-  content::{ADD_CIRCLE, ADD_CIRCLE_OUTLINE, REPORT},
-  image::NAVIGATE_NEXT,
-  navigation::{
-    ARROW_DROP_DOWN, ARROW_DROP_UP, ARROW_LEFT, ARROW_RIGHT, CLOSE, FIRST_PAGE, LAST_PAGE,
-    UNFOLD_MORE, CHEVRON_LEFT
-  },
-  notification::SYNC,
-};
+use druid_widget_nursery::material_icons::IconPaths;
+
+mod icons {
+  pub use druid_widget_nursery::material_icons::normal::{
+    action::{
+      DELETE, EXTENSION, HELP, INSTALL_DESKTOP, OPEN_IN_BROWSER as OPEN_BROWSER, SETTINGS, VERIFIED,
+    },
+    av::{NEW_RELEASES, PLAY_ARROW},
+    content::{ADD_CIRCLE, ADD_CIRCLE_OUTLINE, INVENTORY_2, REPORT},
+    file::FOLDER,
+    image::NAVIGATE_NEXT,
+    navigation::{
+      ARROW_DROP_DOWN, ARROW_DROP_UP, ARROW_LEFT, ARROW_RIGHT, CHEVRON_LEFT, CLOSE, FIRST_PAGE,
+      LAST_PAGE, UNFOLD_MORE,
+    },
+    notification::{SYNC, SYSTEM_UPDATE},
+    toggle::TOGGLE_ON,
+  };
+}
+
+pub const EXTENSION: IconPaths = icons::EXTENSION;
+pub const HELP: IconPaths = icons::HELP;
+pub const INSTALL_DESKTOP: IconPaths = icons::INSTALL_DESKTOP;
+pub const OPEN_BROWSER: IconPaths = icons::OPEN_BROWSER;
+pub const SETTINGS: IconPaths = icons::SETTINGS;
+pub const VERIFIED: IconPaths = icons::VERIFIED;
+pub const NEW_RELEASES: IconPaths = icons::NEW_RELEASES;
+pub const PLAY_ARROW: IconPaths = icons::PLAY_ARROW;
+pub const ADD_CIRCLE: IconPaths = icons::ADD_CIRCLE;
+pub const ADD_CIRCLE_OUTLINE: IconPaths = icons::ADD_CIRCLE_OUTLINE;
+pub const INVENTORY_2: IconPaths = icons::INVENTORY_2;
+pub const REPORT: IconPaths = icons::REPORT;
+pub const NAVIGATE_NEXT: IconPaths = icons::NAVIGATE_NEXT;
+pub const ARROW_DROP_DOWN: IconPaths = icons::ARROW_DROP_DOWN;
+pub const ARROW_DROP_UP: IconPaths = icons::ARROW_DROP_UP;
+pub const ARROW_LEFT: IconPaths = icons::ARROW_LEFT;
+pub const ARROW_RIGHT: IconPaths = icons::ARROW_RIGHT;
+pub const CHEVRON_LEFT: IconPaths = icons::CHEVRON_LEFT;
+pub const CLOSE: IconPaths = icons::CLOSE;
+pub const FIRST_PAGE: IconPaths = icons::FIRST_PAGE;
+pub const LAST_PAGE: IconPaths = icons::LAST_PAGE;
+pub const UNFOLD_MORE: IconPaths = icons::UNFOLD_MORE;
+pub const SYNC: IconPaths = icons::SYNC;
+pub const FOLDER: IconPaths = icons::FOLDER;
+pub const TOGGLE_ON: IconPaths = icons::TOGGLE_ON;
+pub const SYSTEM_UPDATE: IconPaths = icons::SYSTEM_UPDATE;
+pub const DELETE: IconPaths = icons::DELETE;
