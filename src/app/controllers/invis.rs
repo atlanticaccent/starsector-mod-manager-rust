2654b6dc42d4c289a32e1b39e8f47ae01b3a4bec
Iain Laird <iain@laird.org>
2024-05-01 23:56:01 +0100

    Improve invis implementation

--- src/app/controllers/invis.rs
+++ src/app/controllers/invis.rs
@@ -1,8 +1,8 @@
-use druid::{Data, Env, PaintCtx, Widget, WidgetPod};
+use druid::{Data, Env, PaintCtx, UpdateCtx, Widget, WidgetPod};
 use proc_macros::Widget;
 
 #[derive(Widget)]
-#[widget(widget_pod = 1, paint = paint_impl)]
+#[widget(widget_pod = 1, paint = paint_impl, update = update_impl)]
 pub struct InvisibleIf<T, W>(Box<dyn Fn(&T) -> bool>, WidgetPod<T, W>);
 
 impl<T: Data, W: Widget<T>> InvisibleIf<T, W> {
@@ -14,5 +14,12 @@ impl<T: Data, W: Widget<T>> InvisibleIf<T, W> {
     if !self.0(data) {
       self.1.paint(ctx, data, env)
     }
+  }
+
+  fn update_impl(&mut self, ctx: &mut UpdateCtx, old_data: &T, data: &T, env: &Env) {
+    if !old_data.same(data) {
+      ctx.request_paint()
+    }
+    self.1.update(ctx, data, env)
   }
 }
