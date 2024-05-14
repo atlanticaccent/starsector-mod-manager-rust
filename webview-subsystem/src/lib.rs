f50a15a7eb62449b4dbd21455482bc743d271333
Iain Laird <iain@laird.org>
2024-05-14 15:14:03 +0100

    Update druid fork

--- webview-subsystem/src/lib.rs
+++ webview-subsystem/src/lib.rs
@@ -16,43 +16,49 @@ pub fn init_webview(
 
   let webview = WebViewBuilder::new_as_child(window)
     .with_url(url.as_deref().unwrap_or(FRACTAL_INDEX))
+    .with_bounds(wry::Rect {
+      position: wry::dpi::Position::Logical((0., 0.).into()),
+      size: wry::dpi::Size::Logical((0, 0).into()),
+    })
     .with_initialization_script(init_script)
     .with_ipc_handler({
       let ext_ctx = ext_ctx.clone();
       move |req| {
         let string = req.into_body();
         match string.as_str() {
-        _ if string.starts_with("data:") => {
-          let _ = ext_ctx.submit_command_global(WEBVIEW_EVENT, UserEvent::BlobChunk(Some(string)));
-        }
-        "#EOF" => {
-          let _ = ext_ctx.submit_command_global(WEBVIEW_EVENT, UserEvent::BlobChunk(None));
-        }
-        _ if string.starts_with("confirm_download") => {
-          let mut parts = string.split(',');
-          let confirm = parts
-            .next()
-            .expect("split ipc")
-            .split(':')
-            .nth(1)
-            .expect("split ipc");
-          if confirm == "true" {
-            let base = parts
+          _ if string.starts_with("data:") => {
+            let _ =
+              ext_ctx.submit_command_global(WEBVIEW_EVENT, UserEvent::BlobChunk(Some(string)));
+          }
+          "#EOF" => {
+            let _ = ext_ctx.submit_command_global(WEBVIEW_EVENT, UserEvent::BlobChunk(None));
+          }
+          _ if string.starts_with("confirm_download") => {
+            let mut parts = string.split(',');
+            let confirm = parts
               .next()
               .expect("split ipc")
               .split(':')
               .nth(1)
               .expect("split ipc");
-            let decoded = decode(base).expect("decode uri");
-            let uri = String::from_utf8(decoded).expect("decode");
-            let _ = ext_ctx.submit_command_global(WEBVIEW_EVENT, UserEvent::Download(uri));
-          } else {
-            let _ = ext_ctx.submit_command_global(WEBVIEW_EVENT, UserEvent::CancelDownload);
+            if confirm == "true" {
+              let base = parts
+                .next()
+                .expect("split ipc")
+                .split(':')
+                .nth(1)
+                .expect("split ipc");
+              let decoded = decode(base).expect("decode uri");
+              let uri = String::from_utf8(decoded).expect("decode");
+              let _ = ext_ctx.submit_command_global(WEBVIEW_EVENT, UserEvent::Download(uri));
+            } else {
+              let _ = ext_ctx.submit_command_global(WEBVIEW_EVENT, UserEvent::CancelDownload);
+            }
           }
+          _ => {}
         }
-        _ => {}
       }
-    }})
+    })
     .with_navigation_handler({
       let ext_ctx = ext_ctx.clone();
       move |uri: String| {
@@ -104,10 +110,11 @@ pub fn init_webview(
       let ext_ctx = ext_ctx.clone();
       move |_, _| {
         ext_ctx.submit_command_global(WEBVIEW_EVENT, UserEvent::PageLoaded);
-    }})
+      }
+    })
     .build()?;
 
-  #[cfg(debug_assertions)]
+  #[cfg(all(debug_assertions, not(target_os = "macos")))]
   webview.open_devtools();
 
   Ok(webview)
