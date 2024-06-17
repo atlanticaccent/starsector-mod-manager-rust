895373f827df7bb3601fe0c883a00bdda264a652
Iain Laird <iain@laird.org>
2024-06-17 15:51:31 +0100

    Add more card primitives

--- src/widgets/card_button.rs
+++ src/widgets/card_button.rs
@@ -1,5 +1,9 @@
-use druid::{Data, Widget, WidgetExt as _};
+use std::rc::Rc;
 
+use druid::{Data, Selector, Widget, WidgetExt as _, WidgetId};
+use druid_widget_nursery::WidgetExt;
+
+use super::root_stack::RootStack;
 use crate::{
   app::util::{bold_text, WidgetExtEx as _},
   widgets::card::Card,
@@ -38,5 +42,53 @@ impl CardButton {
     inner: impl Fn(bool) -> W + 'static,
   ) -> impl Widget<T> {
     Self::button_unconstrained(inner).fix_height(52.)
+  }
+
+  fn dropdown_maker<W: Widget<crate::app::App> + 'static>(
+    inner: impl Fn(bool) -> W + 'static,
+    width: f64,
+  ) -> impl Fn() -> Box<dyn Widget<crate::app::App>> {
+    let inner = Rc::new(inner);
+    move || {
+      let inner = inner.clone();
+      Self::button_unconstrained(move |hover| (inner.clone())(hover))
+        .fix_width(width)
+        .boxed()
+    }
+  }
+
+  pub fn stacked_dropdown<
+    T: Data,
+    W: Widget<T> + 'static,
+    WO: Widget<crate::app::App> + 'static,
+  >(
+    base: impl Fn(bool) -> W + 'static,
+    dropdown: impl Fn(bool) -> WO + 'static,
+    width: f64,
+  ) -> impl Widget<T> {
+    const DROPDOWN_DISMISSED: Selector = Selector::new("stacked_dropdown.re-enable");
+
+    let id = WidgetId::next();
+    let dropdown = Rc::new(Self::dropdown_maker(dropdown, width));
+
+    Self::button(base)
+      .fix_width(width)
+      .scope_with(false, move |widget| {
+        widget
+          .on_click(move |ctx, data, _| {
+            data.inner = true;
+            let dropdown = dropdown.clone();
+            RootStack::show(
+              ctx,
+              ctx.window_origin(),
+              move || (dropdown)(),
+              Some(move |ctx: &mut druid::EventCtx| ctx.submit_command(DROPDOWN_DISMISSED.to(id))),
+            )
+          })
+          .invisible_if(|data| data.inner)
+          .disabled_if(|data, _| data.inner)
+          .on_command(DROPDOWN_DISMISSED, |_, _, data| data.inner = false)
+          .with_id(id)
+      })
   }
 }
