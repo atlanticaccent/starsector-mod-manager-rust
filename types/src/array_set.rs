c5929f19eaf55cfd6a4eeb31c3a92978e4f81108
Iain Laird <iain@laird.org>
2024-12-01 21:10:49 +0000

    ArraySet tweaks

--- types/src/array_set.rs
+++ types/src/array_set.rs
@@ -29,7 +29,7 @@ impl<T: PartialEq, const CAP: usize> ArraySet<T, CAP> {
   }
 
   pub fn remove(&mut self, val: &T) -> bool {
-    if let Some(idx) = self.index(val) {
+    if let Some(idx) = self.position(val) {
       let last = self.len - 1;
       self.as_mut().swap(idx, last);
       unsafe { MaybeUninit::assume_init_drop(&mut self.mem[last]) };
@@ -45,14 +45,8 @@ impl<T: PartialEq, const CAP: usize> ArraySet<T, CAP> {
     self.as_ref().contains(&val)
   }
 
-  pub fn index(&self, val: &T) -> Option<usize> {
-    for (idx, elem) in self.as_ref().iter().enumerate() {
-      if elem == val {
-        return Some(idx);
-      }
-    }
-
-    None
+  pub fn position(&self, val: &T) -> Option<usize> {
+    self.as_ref().iter().position(|elem| elem == val)
   }
 
   pub fn clear(&mut self) {
@@ -78,14 +72,22 @@ const fn uninit_array<T, const CAP: usize>() -> [MaybeUninit<T>; CAP] {
   [const { MaybeUninit::uninit() }; CAP]
 }
 
-impl<T: PartialEq, const CAP: usize> PartialEq for ArraySet<T, CAP> {
-  fn eq(&self, other: &Self) -> bool {
-    self.len == other.len
-      && self
-        .as_ref()
-        .iter()
-        .zip(other.as_ref())
-        .all(|(left, right)| left == right)
+impl<T: PartialEq, const CAP: usize, const OTHER_CAP: usize> PartialEq<ArraySet<T, OTHER_CAP>>
+  for ArraySet<T, CAP>
+{
+  fn eq(&self, other: &ArraySet<T, OTHER_CAP>) -> bool {
+    if self.len != other.len {
+      return false;
+    }
+
+    let other = other.as_ref();
+    for elem in self.as_ref() {
+      if !other.contains(elem) {
+        return false;
+      }
+    }
+
+    true
   }
 }
 
@@ -145,15 +147,24 @@ where
   type Error = ArraySetError;
 
   fn try_from(value: [T; LEN]) -> Result<Self, Self::Error> {
-    let mut new = Self::default();
-    for (idx, elem) in value.into_iter().enumerate() {
-      for prev in 0..idx {
-        if unsafe { new.mem[prev].assume_init_ref() } == &elem {
+    for (idx, elem) in value.iter().enumerate().skip(1) {
+      for prev in &value[0..idx] {
+        if prev == elem {
           return Err(ArraySetError::ContainsDuplicates);
         }
       }
-      new.mem[idx] = MaybeUninit::new(elem);
     }
+
+    let mut new = Self::default();
+    new.len = LEN;
+
+    // Safety: this cast is safe because we're just casting the pointer to a pointer
+    // of an equivalent type except for the differences in length. Using a different
+    // length is ok because we guarantee LEN is less than or equal to CAP and so
+    // the pointer can't be used to reference into invalid memory whilst
+    // MaybeUninit<T> is guaranteed to have the same alignment and size of T.
+    let mem_ptr = new.mem.as_mut_ptr() as *mut [T; LEN];
+    unsafe { mem_ptr.write(value) };
 
     Ok(new)
   }
@@ -170,15 +181,17 @@ impl<T: Clone + PartialEq, const CAP: usize> TryFrom<&[T]> for ArraySet<T, CAP> 
       });
     }
 
-    let mut new = Self::default();
-    for (idx, elem) in value.iter().enumerate() {
-      for prev in 0..idx {
-        if unsafe { new.mem[prev].assume_init_ref() } == elem {
+    for (idx, elem) in value.iter().enumerate().skip(1) {
+      for prev in &value[0..idx] {
+        if prev == elem {
           return Err(ArraySetError::ContainsDuplicates);
         }
       }
-      new.mem[idx] = MaybeUninit::new(elem.clone());
     }
+
+    let mut new = Self::default();
+    new.len = value.len();
+    MaybeUninit::clone_from_slice(&mut new.mem, value);
 
     Ok(new)
   }
@@ -197,6 +210,7 @@ impl<T, const CAP: usize> Drop for ArraySet<T, CAP> {
 #[cfg(test)]
 mod test {
   use std::{
+    fmt::Debug,
     hash::{BuildHasher, Hasher, RandomState},
     rc::{Rc, Weak},
   };
@@ -247,64 +261,111 @@ mod test {
   struct DropCheck(Rc<u64>);
 
   #[test]
-  fn basic() {
+  fn insert_contains_and_remove() {
     let mut set: ArraySet<usize, 10> = ArraySet::new();
 
     // Can insert 0 to =9
-    for elem in 0..10 {
-      assert!(set.insert(elem));
-      // Set size reported correctly
-      assert_eq!(set.len(), elem + 1);
-      // Cannot insert item that is already in set
-      assert!(!set.insert(elem));
-      // Set size reported correctly
-      assert_eq!(set.len(), elem + 1);
+    let mut insert = || {
+      for elem in 0..10 {
+        assert!(set.insert(elem));
+        // Set size reported correctly
+        assert_eq!(set.len(), elem + 1);
+        // Cannot insert item that is already in set
+        assert!(!set.insert(elem));
+        // Set size reported correctly
+        assert_eq!(set.len(), elem + 1);
 
-      // Set contains all previously inserted items
-      for prev in 0..=elem {
-        assert!(set.contains(&prev))
+        // Set contains all previously inserted items
+        let contains = || {
+          for prev in 0..=elem {
+            assert!(set.contains(&prev))
+          }
+        };
+        contains();
       }
-    }
-    // Set size is 10 after inserting 10 items
-    assert_eq!(set.len(), 10);
+      // Set size is 10 after inserting 10 items
+      assert_eq!(set.len(), 10);
+    };
+    insert();
 
     // Cannot insert into full set
-    assert!(!set.insert(10));
-    // Set size is still 10
-    assert_eq!(set.len(), 10);
+    let mut cannot_insert_into_full = || {
+      assert!(!set.insert(10));
+      // Set size is still 10
+      assert_eq!(set.len(), 10);
+    };
+    cannot_insert_into_full();
 
     // Can remove inserted items
-    for elem in 0..10 {
-      assert!(set.remove(&elem));
-      // Set size reported correctly
-      assert_eq!(set.len(), 9 - elem)
-    }
-    // Set size is 0 after removing all items
-    assert_eq!(set.len(), 0);
+    let mut can_remove_inserted_items = || {
+      for elem in 0..10 {
+        assert!(set.remove(&elem));
+        // Set size reported correctly
+        assert_eq!(set.len(), 9 - elem)
+      }
+      // Set size is 0 after removing all items
+      assert_eq!(set.len(), 0);
+    };
+    can_remove_inserted_items();
 
     // (Set does not contain/cannot remove) previously-inserted-but-removed items
-    for elem in 0..10 {
-      assert!(!set.contains(&elem));
-      assert!(!set.remove(&elem));
+    let mut previously_removed_items_no_longer_present = || {
+      for elem in 0..10 {
+        assert!(!set.contains(&elem));
+        assert!(!set.remove(&elem));
+        // Set size is still 0
+        assert_eq!(set.len(), 0)
+      }
       // Set size is still 0
-      assert_eq!(set.len(), 0)
-    }
-    // Set size is still 0
-    assert_eq!(set.len(), 0);
+      assert_eq!(set.len(), 0);
+    };
+    previously_removed_items_no_longer_present();
+  }
 
-    // Can convert from array
-    let array: [usize; 10] = std::array::from_fn(|i| i);
-    set = ArraySet::try_from(array).expect("Convert");
-    set.clear();
-
-    // Can convert from slice
-    let array: [usize; 10] = std::array::from_fn(|i| i);
-    set = ArraySet::try_from(array.as_slice()).expect("Convert");
+  #[test]
+  fn does_not_contain_unseen_items() {
+    // Setup
+    let mut set: ArraySet<usize, 10> = ArraySet::new();
 
     // Set does not contain items that were never inserted
     for elem in 10..20 {
       assert!(!set.contains(&elem))
     }
+
+    // Insert elements
+    (0..10).for_each(|i| {
+      set.insert(i);
+    });
+
+    // Set still does not contain items that were not inserted
+    for elem in 10..20 {
+      assert!(!set.contains(&elem));
+      assert!(!set.remove(&elem));
+    }
+  }
+
+  #[test]
+  fn conversions() {
+    let array: [usize; 10] = std::array::from_fn(|i| i);
+
+    // Can convert from slice
+    let mut set: ArraySet<usize, 10> = ArraySet::try_from(array.as_slice()).expect("Convert");
+
+    for elem in 0..10 {
+      assert!(set.contains(&elem))
+    }
+
+    // Can convert from array
+    set = ArraySet::try_from(array).expect("Convert");
+
+    for elem in 0..10 {
+      assert!(set.contains(&elem))
+    }
+  }
+
+  #[test]
+  fn clear_and_reuse() {
+    let mut set: ArraySet<usize, 10> = generate(0..10);
 
     // Can clear set
     set.clear();
@@ -326,8 +387,39 @@ mod test {
         assert!(set.contains(&prev))
       }
     }
+  }
 
-    set.clear();
+  fn generate<T: PartialEq, const CAP: usize>(iter: impl IntoIterator<Item = T>) -> ArraySet<T, CAP>
+  where
+    ArraySet<T, CAP>: TryFrom<[T; CAP]>,
+    <ArraySet<T, CAP> as TryFrom<[T; CAP]>>::Error: Debug,
+  {
+    let mut iter = iter.into_iter();
+    let arr: [T; CAP] = std::array::from_fn(|_| iter.next().unwrap());
+
+    ArraySet::try_from(arr).expect("Convert")
+  }
+
+  #[test]
+  fn equality() {
+    let set: ArraySet<usize, 10> = generate(0..10);
+    let mut other: ArraySet<usize, 10> = generate(0..10);
+
+    assert_eq!(set, other);
+
+    other.clear();
+    for elem in (0..10).rev() {
+      other.insert(elem);
+    }
+
+    // Set equality does not respect insertion order
+    assert_eq!(set, other);
+  }
+
+  #[test]
+  fn cloneable() {
+    let mut set: ArraySet<usize, 10> = ArraySet::new();
+
     // Check set is cloneable
     for elem in 0..10 {
       assert!(set.insert(elem));
@@ -335,11 +427,21 @@ mod test {
       let other = set.clone();
       assert_eq!(set, other)
     }
+  }
 
+  #[test]
+  fn drop_check() {
     let mut set: ArraySet<DropCheck, 10> = ArraySet::new();
-    let mut tracker = DropTracker::new();
 
+    let mut tracker = DropTracker::new();
     (0..10).for_each(|_| assert!(set.insert(tracker.build())));
+    assert!(tracker.all_live());
+
+    drop(set);
+    assert!(tracker.all_dead());
+
+    let mut tracker = DropTracker::new();
+    let set: ArraySet<DropCheck, 10> = generate((0..10).map(|_| tracker.build()));
     assert!(tracker.all_live());
 
     drop(set);
