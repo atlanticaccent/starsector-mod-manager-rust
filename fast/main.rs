eb09c43592895c963e6c3eb6453fd8f61b76c859
Iain Laird <iain@laird.org>
2024-12-05 22:57:46 +0000

    Fix fast build aliases on windows

--- fast/main.rs
+++ fast/main.rs
@@ -7,9 +7,13 @@ fn main() {
   cmd.args(args);
   // if args...contains... "--target" "...macos..." ...
   if cfg!(target_os = "macos") {
-    cmd.arg("--profile=iter-mac");
+    cmd.arg("--profile=fast-macos");
+  } else if cfg!(target_os = "windows") {
+    cmd.arg("--profile=fast");
+    cmd.arg("--config");
+    cmd.arg(r#"profile.fast.rustflags = ["-C", "embed-bitcode=no", "-C", "lto=off", "-Z", "share-generics=true", "-C", "target-feature=-crt-static"]"#);
   } else {
-    cmd.arg("--profile=iter");
+    cmd.arg("--profile=fast");
   }
   if cmd.status().ok().map_or(false, |it| it.success()).not() {
     process::exit(-1);
