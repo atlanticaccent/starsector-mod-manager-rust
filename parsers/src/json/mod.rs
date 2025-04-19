3c63eb9823c6de63620aeea8525fd3522e03e715
Iain Laird <iain@laird.org>
2025-04-19 22:59:28 +0100

    Refactor winnow_json into parsers crate

---
+++ parsers/src/json/mod.rs
@@ -0,0 +1,13 @@
+mod parser_dispatch;
+
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Clone)]
+pub(crate) enum JsonValue {
+  Null,
+  Boolean(bool),
+  Str(String),
+  Num(f64),
+  Array(Vec<JsonValue>),
+  Object(HashMap<String, JsonValue>),
+}
--- winnow_json/src/lib.rs
+++
@@ -1,4 +0,0 @@
-#[path = "winnow/examples/json/json.rs"]
-mod json;
-#[path = "winnow/examples/json/parser_dispatch.rs"]
-mod parser_dispatch;
